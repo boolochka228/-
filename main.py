5dcf1a350d339b4589a68ac21160a86d483824e4
boolochka228 <arturus697@gmail.com>
2026-06-13 17:49:15 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,33 @@
+from pygame import *
+from settings import *
+from sounds import load_sounds
+from keys import draw_keys, create_key_rects
 
+init()
+screen = display.set_mode ((WINDOW_WIDTH, WINDOW_HEIGHT))
+display.set_caption("Piano Game")
+
+sounds = load_sounds(KEYS)
+key_rects = create_key_rects(len(KEYS))
+keys_list = list(KEYS.key())
+my_font = font.SysFont("Arial", 24)
+pressed_keys = set()
+
+# кнопки меню
+def start_game (): pass
+def open_settings(): pass
+def exit_game(): quit()
+
+buttons = [
+  Buttons(60, 20, 120, 40, "Settings",  open_settings)
+]
+running = True 
+while running: 
+  screen. fill(WHITE)
+
+#кнопки
+for button in buttons:
+  buttton.draw(screen, my_font)
+
+#клавіши
+draw_keys(screen
