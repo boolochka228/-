c7fc7ae4936127a37a9c5b9f3b568b88c2c41e4b
boolochka228 <arturus697@gmail.com>
2026-06-20 16:43:41 +0300

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,1 +1,45 @@
+> Artur U:
+🥸 Sticker
+
+> nastyusha:
+draw_keys(screen, key_rects, pressed_keys)
+
+    display.flip()
+
+    for e in event.get():
+        if e.type == QUIT:
+            running = False
+
+        # кнопки
+        for button in buttons:
+            button.handle_event(e)
+
+        # клавіатура
+        if e.type == KEYDOWN:
+            k = key.name(e.key)
+            if k in sounds:
+                sounds[k].play()
+                idx = keys_list.index(k)
+                pressed_keys.add(idx)
+
+        if e.type == KEYUP:
+            k = key.name(e.key)
+            if k in sounds:
+                idx = keys_list.index(k)
+                if idx in pressed_keys:
+                    pressed_keys.remove(idx)
+
+        # миша по клавішах
+        if e.type == MOUSEBUTTONDOWN:
+            pos = e.pos
+            for i, rect in enumerate(key_rects):
+                if rect.collidepoint(pos):
+                    sounds[keys_list[i]].play()
+                    pressed_keys.add(i)
+
+        if e.type == MOUSEBUTTONUP:
+            pos = e.pos
+            for i, rect in enumerate(key_rects):
+                if i in pressed_keys and rect.collidepoint(pos):
+                    pressed_keys.remove(i)
 
